9407f0d8bb97e25960aba00eeb46f9319cdb69f8
Gabrielle DeWitt <gabrielle.dewitt@life.io>
2017-03-23 16:07:25 -0700

    Pass dynamic callback url to Garmin

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -26,6 +26,7 @@ module OmniAuth
       end
 
       def consumer
+        options.client_options["authorize_url"] += '?oauth_callback=' + request.base_url + '/auth/garmin/callback'
         consumer = GarminConsumer.new(options.consumer_key, options.consumer_secret, options.client_options)
         consumer.http.open_timeout = options.open_timeout if options.open_timeout
         consumer.http.read_timeout = options.read_timeout if options.read_timeout
